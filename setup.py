6e8fe13e58f1abb39256ccac7dc1920e7bdd9c97
Samuele Santi <redshadow@hackzine.org>
2012-06-03 22:04:16 +0200

    Added distutil's setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Config-gen',
+    version='0.1',
+    description='Utility to generate configuration files',
+    author='Samuele Santi',
+    author_email='samuele@santi.co.it',
+    url='https://github.com/rshk/config-gen',
+    scripts=['gencfg.py'],
+    #data_files=['README.rst'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Topic :: Utilities",
+    ],
+)
