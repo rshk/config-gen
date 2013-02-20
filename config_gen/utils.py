50f0182eff06b67e77718b3d3dc73075072137cd
Samuele Santi <redshadow@hackzine.org>
2013-02-21 00:52:31 +0100

    Now caching readers to avoid re-instantiating over and over

--- config_gen/utils.py
+++ config_gen/utils.py
@@ -19,10 +19,13 @@ class GenericRegister(dict):
 class LazyRegister(object, DictMixin):
     def __init__(self):
         self._register = {}
+        self._cache = {}
 
     def __getitem__(self, item):
-        record = self._register[item]
-        return record[0](*record[1], **record[2])
+        if item not in self._cache:
+            record = self._register[item]
+            self._cache[item] = record[0](*record[1], **record[2])
+        return self._cache[item]
 
     def keys(self):
         return self._register.keys()
