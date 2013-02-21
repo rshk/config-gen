17bc1d2e9cf05e5dc3a9479451051ab7c430b13b
Samuele Santi <redshadow@hackzine.org>
2013-02-21 01:51:07 +0100

    refactoring

--- config_gen/commands/quickstart.py
+++ config_gen/commands/quickstart.py
@@ -9,7 +9,22 @@ from cool_logging import getLogger
 logger = getLogger('config-gen')
 
 
-MAKEFILE = """\
+STANDARD_DIRS = [
+    'templates',
+    'extra_templates',
+    'data',
+    'build',  # No real need..
+]
+
+STANDARD_FILES = {}
+
+STANDARD_FILES['templates/example.html.jinja'] = \
+    '<h1>{{ example.hello_msg }}</h1>\n'
+STANDARD_FILES['data/example.json'] = \
+    '{"hello_msg": "Hello, world!"}\n'
+
+
+STANDARD_FILES['Makefile'] = """\
 ## Makefile for config-gen
 
 .PHONY: all clean
@@ -18,24 +33,27 @@ all:
 \tconfgen-render --root=.
 
 clean:
-\trm -f build/*
+\trm -f build
+"""
 
+STANDARD_FILES['.gitignore'] = """\
+## Config-gen ignored files
+*~
+*.pyc
+/build/*
 """
 
 
 def command():
     root_dir = os.getcwd()
-    relfile = lambda name: os.path.join(root_dir, name)
-    os.makedirs(relfile('templates'))
-    os.makedirs(relfile('data'))
-    os.makedirs(relfile('build'))
-    os.makedirs(relfile('extra_templates'))
-    with open(relfile('Makefile'), 'w') as f:
-        f.write(MAKEFILE)
-    with open(relfile('templates/example.html.jinja'), 'w') as f:
-        f.write('<h1>{{ example.hello_msg }}</h1>\n')
-    with open(relfile('data/example.json'), 'w') as f:
-        f.write('{"hello_msg": "Hello, world!"}\n')
+
+    for dirname in STANDARD_DIRS:
+        os.makedirs(os.path.join(root_dir, dirname))
+
+    for file_name, file_content in STANDARD_FILES.iteritems():
+        with open(file_name, 'w') as f:
+            f.write(file_content)
+
     print "Done. Now run 'make' to compile an example file."
 
 if __name__ == '__main__':
